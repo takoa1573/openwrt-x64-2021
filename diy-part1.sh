d73260f27c2c63f90c6d0461d974684161bb7a3f
takoa1573 <58153939+takoa1573@users.noreply.github.com>
2021-12-13 22:17:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,5 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+#echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
